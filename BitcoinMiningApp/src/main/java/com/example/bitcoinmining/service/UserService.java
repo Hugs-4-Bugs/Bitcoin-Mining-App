dfa84a0c2665d6c432e04035ffcdf19ccc5856c6
Prabhat Kumar <prabhathkumar.cse@skit.org.in>
2024-03-29 05:44:36 +0530

    Add files via upload

---
+++ BitcoinMiningApp/src/main/java/com/example/bitcoinmining/service/UserService.java
@@ -0,0 +1,10 @@
+package com.example.bitcoinmining.service;
+
+import com.example.bitcoinmining.model.User;
+
+public interface UserService {
+    User registerUser(User user);
+    // Add more user service methods as needed
+
+	void registerUser(String username, String encodedPassword);
+}
